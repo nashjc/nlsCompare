54287644e3a445653e3d3d0a31474c816bc259b2
john <nashjc@uottawa.ca>
2022-06-21 15:52:54 -0400

    Start of effort to add NISTnls problems

---
+++ inst/scripts/Bennett5_2.R
@@ -0,0 +1,38 @@
+# NLSProbName: Bennett5_2.R
+# NLSProbDescription: {These data are the result of a NIST study involving
+#               superconductivity magnetization modeling.  The
+#               response variable is magnetism, and the predictor
+#               variable is the log of time in minutes.
+#
+#Reference:     Bennett, L., L. Swartzendruber, and H. Brown, 
+#               NIST (1994).  
+#               Superconductivity Magnetization Modeling.
+# }
+
+## DATA
+# NOTE: we can get the data frame directly from the NISTnls package if installed.
+NLSdata <- NISTnls::Bennett5
+
+## STARTING VALUE
+b1 <- -1500; b2 <- 45; b3 <- 0.85
+NLSstart <-list(b1=b1, b2=b2, b3=b3) # a starting vector (named!)
+
+## MODEL
+NLSformula <- y ~ b1*(b2+x)**(-1/b3)
+NLSlower<- c(-Inf,-Inf,-Inf)
+NLSupper<- c(Inf,Inf,Inf)
+NLSsubset <- 1:length(y)
+NLSweights <- rep(1,length(y))
+NLSref<-"nlsr"
+#NLSrefVersion<-packageVersion(NLSref)
+refsol<-nlsr::nlxb(NLSformula,NLSstart,NLSdata,lower=NLSlower, upper=NLSupper,trace=TRUE)
+NLSproblems <- read.table(system.file("extdata","problems.csv",
+                                        package="nlsCompare"),header=TRUE,sep=",")
+NLSpars<-as.numeric(subset(NLSproblems,Name=="Bennett5_2.R")[4:(4+subset(NLSproblems,Name=="Bennett5_2.R")$nPars-1)])
+NLSssquares<-subset(NLSproblems,Name=="Bennett5_2.R")$ssquares
+NLStag<-"unbounded"
+# NLSproblems[NLSproblems[,"Name"]=="Bennett5_2.R",]$PkgVers<-paste(NLSref,NLSrefVersion,sep=":")
+# NLSproblems[NLSproblems[,"Name"]=="Bennett5_2.R",]$LastUpdated<-format(Sys.time(), "%Y-%m-%d %H:%M")
+# write.table(NLSproblems,system.file("extdata","problems.csv",
+#                                         package="nlsCompare"),append=FALSE,sep=",",col.names=FALSE,row.names=FALSE)
+rm(y,tt,b1,b2,b3)
